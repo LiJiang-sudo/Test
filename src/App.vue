753b9c6319ede9d659276095a2c6bd22470e4472
xiaoli <2226450987@qq.com>
2021-07-16 16:59:59 +0800

    logs

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <child v-bind:msg="ool" v-on:father="showFather"></child>
+    {{ show }}
+  </div>
+</template>
+
+<script>
+import child from "./components/Child.vue";
+export default {
+  name: "app",
+  components: {
+    child,
+  },
+  data() {
+    return {
+      ool: "弄死恁爹 我是你大爷",
+      show: "",
+    };
+  },
+  methods: {
+    showFather: function (data) {
+      this.show = data;
+    },
+  },
+};
+</script>
---
+++ src/components/Child.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Child子组件部分</h2>
+        <p>{{msg}}</p>
+        <button v-on:click="sendMsg">向父组件传值</button>
+    </div>
+</template>
+<script>
+export default {
+    props:['msg'],
+    methods:{
+        sendMsg:function(){
+            this.$emit("father",'去微软推哦怕');
+        }
+    }
+}
+</script>
